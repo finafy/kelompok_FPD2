19533661e99ec3894f3a67b98ebd5f213fbe4d10
nuzhamusyafira <nuzhamusyafira@gmail.com>
2017-12-01 16:05:56 +0700

    cp-R

--- cp.c
+++ cp.c
@@ -25,33 +25,33 @@ void cpnew(char *src, char *dst){
 		fclose(in);
 		fclose(out);
 }
-/*void cpdir(char *argv, char *d1){
+void cpdir(char *src, char *dst){
 	int i;
 	DIR *dir;
 	struct dirent *ent;
-	if((dir=opendir(argv))!=NULL){
+	if((dir=opendir(src))!=NULL){
+		char dnew[1000];
+		sprintf(dnew,"%s/%s",dst,src);
+		mkdir(dnew,0777);
 		while((ent=readdir(dir))!=NULL){
 			if(strcmp(ent->d_name,".")==0) continue;
 			else if(strcmp(ent->d_name,"..")==0) continue;
 			else{
 				if(ent->d_type==DT_DIR){
-					char new[1000];
-					sprintf(new,"%s/%s",d1,ent->d_name);
-					mkdir(new,0777);
 					char next[1000];
-					strcpy(next,argv);
-					strcat(next,"/");
-					strcat(next,ent->d_name);
-					cpdir(next,new);
+					sprintf(next,"%s/%s",src,ent->d_name);
+					cpdir(next,dst);
+					continue;
 				}
 				else{
-					cp(ent->d_name,d1);
+					cp(ent->d_name,dnew);
+					continue;
 				}
 			}
 		}
 		closedir(dir);
 	}
-}*/
+}
 void main(int argc, char *argv[]){
 	if(argc<=2){
 		printf("cp: cannot proceed, need more arguments\n");
