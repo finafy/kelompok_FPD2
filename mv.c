b0e843b1976aa6ce6fd8dd34e25d29696c8cd277
nuzhamusyafira <nuzhamusyafira@gmail.com>
2017-12-09 12:52:00 +0700

    nuzha

--- mv.c
+++ mv.c
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int main (int argc, char *argv[]){
-  if (argc != 3){
-    printf(2, "Usage: mv oldname newname\n");
-    exit();
-  }
-
-  if ((link(argv[1], argv[2]) < 0) || (unlink(argv[1]) < 0))
-    printf(2, "mv %s to %s failed\n", argv[1], argv[2]);
-else if(argc>=4){
-for(int i=1;i<argc;i++){
-if ((link(argv[i], argv[argc-1]) < 0) || (unlink(argv[i]) < 0))
-    printf(2, "mv %s to %s failed\n", argv[i], argv[argc-1]);
-}
-}
-  exit();
-=======
 #include<stdio.h>
 #include<string.h>
 #include<dirent.h>
@@ -104,5 +83,4 @@ void main(int argc, char *argv[]){
 		fclose(out);
 		remove(argv[1]);
 	}
->>>>>>> 3089b28e93b5aed9feb404b9ed4545f7f3a6b90e
 }
