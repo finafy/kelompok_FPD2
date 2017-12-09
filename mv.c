86a6f14ab2494f188a28d87d074b0a5e1e507526
nuzhamusyafira <nuzhamusyafira@gmail.com>
2017-12-09 09:51:41 +0700

    gabisa del tp kemove

--- mv.c
+++ mv.c
@@ -1,6 +1,57 @@
 #include<stdio.h>
 #include<string.h>
 #include<dirent.h>
+#include<sys/stat.h>
+void mv(char *src, char *dst){
+		FILE *in,*out;
+		in=fopen(src,"r");
+		char copy[1000];
+		sprintf(copy,"%s/%s",dst,src);
+		out=fopen(copy,"w+");
+		if(in==NULL){
+			fclose(out);
+			remove(src);
+			return;
+		}
+		else{
+			char str;
+			while((str=fgetc(in))!=EOF) fputc(str,out);
+			fclose(in);
+			fclose(out);
+			remove(src);
+			return;
+		}
+}
+void mvdir(char *src, char *dst){
+	int i;
+	DIR *dir;
+	struct dirent *ent;
+	if((dir=opendir(src))!=NULL){
+		char dnew[1000];
+		sprintf(dnew,"%s/%s",dst,src);
+		mkdir(dnew,0777);
+		while((ent=readdir(dir))!=NULL){
+			if(strcmp(ent->d_name,".")==0) continue;
+			else if(strcmp(ent->d_name,"..")==0) continue;
+			else{
+				char now[1000];
+				sprintf(now,"%s",ent->d_name);
+				if(ent->d_type==DT_DIR){
+					char next[1000];
+					sprintf(next,"%s/%s",src,now);
+					mvdir(next,dst);
+					continue;
+				}
+				else{
+					mv(now,dnew);
+					continue;
+				}
+			}
+		}
+		closedir(dir);
+	}
+	return;
+}
 void main(int argc, char *argv[]){
 	FILE *in, *out;
 	DIR *dir;
@@ -15,15 +66,12 @@ void main(int argc, char *argv[]){
 				printf("mv: cannot move '%s' to a subdirectory of itself, %s/%s\n", argv[i],argv[i],argv[i]);
 				continue;
 			}
-			in=fopen(argv[i],"r");
-			char new[2000];
-			sprintf(new,"%s/%s",argv[argc-1],argv[i]);
-			out=fopen(new,"w+");
-			char c;
-			while((c=fgetc(in))!=EOF) fputc(c,out);
-			fclose(in);
-			fclose(out);
-			remove(argv[i]);
+			if((dir=opendir(argv[i]))!=NULL){
+				closedir(dir);
+				mvdir(argv[i],argv[argc-1]);
+				remove(argv[i]);
+			}
+			else mv(argv[i],argv[argc-1]);
 		}
 	}
 	else{
